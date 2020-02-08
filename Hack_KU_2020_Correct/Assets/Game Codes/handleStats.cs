b9f6aeebc8aa1bf94413142def5ccb4dc7c304c3
Sean Cunningham <sean.cunningham360@gmail.com>
2020-02-08 13:51:33 -0600

    User Stats UI

--- Hack_KU_2020_Correct/Assets/Game Codes/handleStats.cs
+++ Hack_KU_2020_Correct/Assets/Game Codes/handleStats.cs
@@ -7,13 +7,20 @@ public class handleStats : MonoBehaviour
 {
    public PlayerStat Funds;
    public PlayerStat Power;
-   
+   public PlayerStat Population;
+   public PlayerStat InPerMin;
+
    public GameObject FundStat;
    public GameObject PowerStat;
+   public GameObject PopuStat;
+   public GameObject IPMStat;
+   
     void Start()
     {
         writeUpdatedText(Funds, FundStat, "Funds: ");
         writeUpdatedText(Power, PowerStat, "Power: ");
+        writeUpdatedText(Population, PopuStat, "Population: ");
+        writeUpdatedText(InPerMin, IPMStat, "Income Per Minute: ");
     }
 
     private void writeUpdatedText(PlayerStat _stat, GameObject _object, string _text) {
