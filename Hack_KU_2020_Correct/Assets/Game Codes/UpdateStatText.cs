2a66236273d2c28ae8b5d1d51f1a21801f72c60d
Sean Cunningham <sean.cunningham360@gmail.com>
2020-02-08 11:38:12 -0600

    Added a statistic handler object which can update the UI to reflect correct stat counts

---
+++ Hack_KU_2020_Correct/Assets/Game Codes/UpdateStatText.cs
@@ -0,0 +1,18 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using Sean.CharacterStats;
+
+public class UpdateStatText : MonoBehaviour
+{
+    private Text _text;
+
+    void Awake() {
+        _text = GetComponent<Text>();
+    }
+
+    public void updateText(string input) {
+        _text.text = input;
+    }
+}
---
+++ Hack_KU_2020_Correct/Assets/Game Codes/handleStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sean.CharacterStats;
+
+public class handleStats : MonoBehaviour
+{
+   public PlayerStat Funds;
+   
+   public GameObject FundStat;
+    void Start()
+    {
+        FundStat.GetComponent<UpdateStatText>().updateText("Funds: "+Funds.Value);
+    }
+
+}
