000f5b6f86811cec40931e63232903382d7f3c70
Sean Cunningham <sean.cunningham360@gmail.com>
2020-02-08 14:27:39 -0600

    File upload

---
+++ Hack_KU_2020_Correct/Assets/Game Codes/ExitButtonClickBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonClickBehaviour : MonoBehaviour
+{
+    public GameObject ExitConfirmationButton;
+    public void openExitMenu() {
+        ExitConfirmationButton.SetActive(true);
+    }
+}
