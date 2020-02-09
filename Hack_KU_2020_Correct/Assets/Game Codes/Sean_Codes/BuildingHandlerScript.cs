a8586ec441c65a34354443da2eadd88c0563317e
coolhead57 <43939935+coolhead57@users.noreply.github.com>
2020-02-09 00:10:32 -0600

    changed building.building

--- Hack_KU_2020_Correct/Assets/Game Codes/Sean_Codes/BuildingHandlerScript.cs
+++ Hack_KU_2020_Correct/Assets/Game Codes/Sean_Codes/BuildingHandlerScript.cs
@@ -6,27 +6,27 @@ public class BuildingHandlerScript : MonoBehaviour
 {
     public BuildingHandlerScript(){}
 
-    public float GetPrices(Building.building)
+    public float GetPrices(Building prices)
     {
       return(Building.GetBuildingprice());
     }
 
-    public float Getpower(Building.building)
+    public float Getpower(Building power)
     {
       return(Building.GetBuildingpower());
     }
 
-    public float Getpop(Building.building)
+    public float Getpop(Building Population)
     {
       return(Building.GetBuildingpopulation());
     }
 
-    public float Getincome(Building.building)
+    public float Getincome(Building income)
     {
       return(Building.GetBuildingincome());
     }
 
-    public GameObject GetBuilding(Building.building)
+    public GameObject GetBuilding(Building BHSbuilding)
     {
       return(Building.GettheBuilding());
     }
