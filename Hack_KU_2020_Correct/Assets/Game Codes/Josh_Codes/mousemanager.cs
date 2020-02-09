d68359d61e3956b6e006f2003b21169d7cd6f93b
Sean Cunningham <sean.cunningham360@gmail.com>
2020-02-09 04:46:00 -0600

    code

--- Hack_KU_2020_Correct/Assets/Game Codes/Josh_Codes/mousemanager.cs
+++ Hack_KU_2020_Correct/Assets/Game Codes/Josh_Codes/mousemanager.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sean;
 
 public class mousemanager : MonoBehaviour
 {
     void Start()
     {
-      /*for (int i = 0; i < Buildingparts.Length; i++)
-      {
-        GameObject Buildings = Buildingparts[i];
-        GameObject buttonGameObject = (GameObject)Instantiate(Buildingparts, this.transform);
-        theButton.onClick.AddListener( () => {Spawnthings = Buildings;} );
-      }*/
+        /*for (int i = 0; i < Buildingparts.Length; i++)
+        {
+          GameObject Buildings = Buildingparts[i];
+          GameObject buttonGameObject = (GameObject)Instantiate(Buildingparts, this.transform);
+          theButton.onClick.AddListener( () => {Spawnthings = Buildings;} );
+        }*/
     }
 
     [SerializeField]
@@ -22,119 +23,129 @@ public class mousemanager : MonoBehaviour
 
     private GameObject CurrentPlaceableObject;
     private float mousewheelrotation = 10f;
-//-------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------
     public GameObject[] Buildingparts;
 
+    private GameObject _spawnthings;
 
     public void setspawnthing(GameObject inputbulding)
     {
-      Spawnthings = inputbulding;
-      print(Spawnthings);
+        _spawnthings = inputbulding;
+        print(_spawnthings);
+    }
+
+    public mousemanager()
+    {
+        _spawnthings = Spawnthings;
+    }
+
+    void Start(){
+      _spawnthings = Spawnthings;
     }
     // Update is called once per frame
     private void Update()
     {
-    /*    //has the mouse been click
-        if (Input.GetMouseButtonDown(0) && !(Input.GetKey(KeyCode.C)))
-        {
-          //button clicked
-          //is it over somthing
+        /*    //has the mouse been click
+            if (Input.GetMouseButtonDown(0) && !(Input.GetKey(KeyCode.C)))
+            {
+              //button clicked
+              //is it over somthing
 
-          Camera theCamera = Camera.main;
+              Camera theCamera = Camera.main;
 
-          Ray ray = theCamera.ScreenPointToRay(Input.mousePosition );
-          RaycastHit hitInfo;
+              Ray ray = theCamera.ScreenPointToRay(Input.mousePosition );
+              RaycastHit hitInfo;
 
-          if (Physics.Raycast(ray, out hitInfo))
-          {
-            //Debug.Log("yes, hit " + hitInfo.collider.gameObject.name);
-            //spawn new object
+              if (Physics.Raycast(ray, out hitInfo))
+              {
+                //Debug.Log("yes, hit " + hitInfo.collider.gameObject.name);
+                //spawn new object
 
-            Vector3 spawnspot = hitInfo.collider.transform.position + hitInfo.normal;
+                Vector3 spawnspot = hitInfo.collider.transform.position + hitInfo.normal;
 
-            Instantiate(Spawnthings,spawnspot, Quaternion.identity);
-          }
-        }
-*/
+                Instantiate(Spawnthings,spawnspot, Quaternion.identity);
+              }
+            }
+    */
         if (Input.GetKey(KeyCode.C) && Input.GetMouseButtonDown(0))
         {
-          Camera theCamra = Camera.main;
-           RaycastHit hit;
-           Ray ray = theCamra.ScreenPointToRay(Input.mousePosition );
-           if (Physics.Raycast(ray, out hit))
-           {
-             Debug.Log("yes, hit " + hit.collider.gameObject.name);
-             BoxCollider bc = hit.collider as BoxCollider;
-             if (bc != null )
-             {
-               Destroy(bc.gameObject);
-             }
-           }
+            Camera theCamra = Camera.main;
+            RaycastHit hit;
+            Ray ray = theCamra.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("yes, hit " + hit.collider.gameObject.name);
+                BoxCollider bc = hit.collider as BoxCollider;
+                if (bc != null)
+                {
+                    Destroy(bc.gameObject);
+                }
+            }
         }
 
         HandleNewObjectHotKey();
         if (CurrentPlaceableObject != null)
         {
-          moveObjecttomouse();
-          Rotatefrommousewheel();
-          ReleaseIfClicked();
+            moveObjecttomouse();
+            Rotatefrommousewheel();
+            ReleaseIfClicked();
         }
     }
 
     private void HandleNewObjectHotKey()
     {
-      if (Input.GetKeyDown(newObjectHotKey))
-      {
-        if (CurrentPlaceableObject != null)
+        if (Input.GetKeyDown(newObjectHotKey))
         {
-            Destroy(CurrentPlaceableObject);
+            if (CurrentPlaceableObject != null)
+            {
+                Destroy(CurrentPlaceableObject);
+            }
+            else
+            {
+                CurrentPlaceableObject = Instantiate(_spawnthings);
+            }
         }
-        else
-        {
-          CurrentPlaceableObject = Instantiate(Spawnthings);
-        }
-      }
     }
 
     private void moveObjecttomouse()
     {
-      CurrentPlaceableObject.layer = 2;
+        CurrentPlaceableObject.layer = 2;
 
-      Camera theCamera = Camera.main;
+        Camera theCamera = Camera.main;
 
-      Ray ray = theCamera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = theCamera.ScreenPointToRay(Input.mousePosition);
 
-      RaycastHit hitInfo;
-      if (Physics.Raycast(ray, out hitInfo))
-       {
-        /* Vector3 spawnspot = hitInfo.collider.transform.position + hitInfo.normal;
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            /* Vector3 spawnspot = hitInfo.collider.transform.position + hitInfo.normal;
 
-         Instantiate(Spawnthings,spawnspot, Quaternion.identity);
-         */
-        CurrentPlaceableObject.transform.position = hitInfo.point;
-        CurrentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
-      }
+             Instantiate(Spawnthings,spawnspot, Quaternion.identity);
+             */
+            CurrentPlaceableObject.transform.position = hitInfo.point;
+            CurrentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
     }
 
     private void Rotatefrommousewheel()
     {
-      //Debug.Log(Input.mouseScrollDelta);
-      mousewheelrotation += Input.mouseScrollDelta.y;
-      CurrentPlaceableObject.transform.Rotate(Vector3.up, mousewheelrotation * 10f);
+        //Debug.Log(Input.mouseScrollDelta);
+        mousewheelrotation += Input.mouseScrollDelta.y;
+        CurrentPlaceableObject.transform.Rotate(Vector3.up, mousewheelrotation * 10f);
     }
 
     private void ReleaseIfClicked()
     {
-      if (Input.GetMouseButtonDown(0))
-      {
-        CurrentPlaceableObject.layer = 0;
-        CurrentPlaceableObject = null;
-      }
+        if (Input.GetMouseButtonDown(0))
+        {
+            CurrentPlaceableObject.layer = 0;
+            CurrentPlaceableObject = null;
+        }
     }
 
     private void onCollisionEnter(Collision collision)
     {
-      Debug.Log("Collided With " + collision.gameObject.name);
-      
+        Debug.Log("Collided With " + collision.gameObject.name);
+
     }
 }
