2c270acb35cf966dc686485f4fc693cd7130a850
coolhead57 <43939935+coolhead57@users.noreply.github.com>
2020-02-09 00:03:33 -0600

    working on building handler

--- Hack_KU_2020_Correct/Assets/Game Codes/Sean_Codes/BuildingHandlerScript.cs
+++ Hack_KU_2020_Correct/Assets/Game Codes/Sean_Codes/BuildingHandlerScript.cs
@@ -5,4 +5,29 @@ using UnityEngine;
 public class BuildingHandlerScript : MonoBehaviour
 {
     public BuildingHandlerScript(){}
+
+    public float GetPrices(Building.building)
+    {
+      return(Building.GetBuildingprice());
+    }
+
+    public float Getpower(Building.building)
+    {
+      return(Building.GetBuildingpower());
+    }
+
+    public float Getpop(Building.building)
+    {
+      return(Building.GetBuildingpopulation());
+    }
+
+    public float Getincome(Building.building)
+    {
+      return(Building.GetBuildingincome());
+    }
+
+    public GameObject GetBuilding(Building.building)
+    {
+      return(Building.GettheBuilding());
+    }
 }
