43f95766b8b5c5db27fcad92c7a1154aa34bfc5b
Sean Cunningham <sean.cunningham360@gmail.com>
2020-02-09 01:36:57 -0600

    Created BuildingType ENUM

--- Hack_KU_2020_Correct/Assets/Game Codes/Sean_Codes/BuildingHandlerScript.cs
+++ Hack_KU_2020_Correct/Assets/Game Codes/Sean_Codes/BuildingHandlerScript.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using UnityEngine;
 
+public enum BuildingType
+    {
+        Road_Straight,
+        Road_Cross,
+        Road_Tee,
+        Road_Corner,
+        House,
+        House_Alt,
+        Skyscraper,
+        Coal_Power,
+        Power_Line,
+        Warehouse,
+        Watertower,
+        Appartment,
+    }
 public class BuildingHandlerScript : MonoBehaviour
 {
 
+    public GameObject PlacementObject;
+    private mousemanager placementManager;
     private readonly List<Building> _buildingList;
     public readonly ReadOnlyCollection<Building> BuildingList;
 
@@ -13,6 +30,11 @@ public class BuildingHandlerScript : MonoBehaviour
     {
         _buildingList = new List<Building>();
         BuildingList = _buildingList.AsReadOnly();
+        placementManager = PlacementObject.GetComponent<mousemanager>();
+    }
+
+    private void updateCurrentPlacementBuilding(GameObject _object){
+        placementManager.setspawnthing(_object);
     }
 
 public float GetPrices(Building _building)
@@ -44,5 +66,6 @@ public void addBuilding(Building _building) {
     _buildingList.Add(_building);
 }
 
+public void shopCheckout()
 
 }
