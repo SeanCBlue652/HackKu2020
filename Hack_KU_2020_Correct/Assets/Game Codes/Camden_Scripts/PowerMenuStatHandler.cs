564f29fdc6fabb6cc2358799a6e9acb7f4c8292b
NanitheCaknupp <camden.knupp@gmail.com>
2020-02-09 02:34:41 -0600

    Power Information Menu

--- Hack_KU_2020_Correct/Assets/Game Codes/Camden_Scripts/PowerMenuStatHandler.cs
+++ Hack_KU_2020_Correct/Assets/Game Codes/Camden_Scripts/PowerMenuStatHandler.cs
@@ -10,7 +10,9 @@ public class PowerMenuStatHandler : MonoBehaviour
 {
     public GameObject PowerStatHandler;
     private handleStats Camden;
-    public Text textBox;
+    public Text GainTxtBox;
+    public Text LossTxtBox;
+    public Text NetTxtBox;
 
     private ReadOnlyCollection<StatModifier> _mods;
 
@@ -25,7 +27,8 @@ public class PowerMenuStatHandler : MonoBehaviour
     }
 
     
-    private float power_Gain = 0;
+    private float powerGain = 0;
+    private float powerLoss = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -34,22 +37,33 @@ public class PowerMenuStatHandler : MonoBehaviour
         Camden = PowerStatHandler.GetComponent<handleStats>();
         Camden.updatePower(StatModType.Flat, 1f);
         Camden.updatePower(StatModType.Flat, 2f);
+        Camden.updatePower(StatModType.Flat, 3f);
+        Camden.updatePower(StatModType.Flat, -4f);
         _mods = Camden.getStatModifiers("Power");
         for (int i = 0; i < _mods.Count; i++)
         {
-            
+            if(_mods[i].Value >= 0)
+            {
+                powerGain += _mods[i].Value;
+            }
+            if(_mods[i].Value <= 0)
+            {
+                powerLoss += _mods[i].Value;
+            }
         }
         updateText();
     }
     
     void awake()
     {
-        textBox = GetComponent<Text>();
+        NetTxtBox = GetComponent<Text>();
     }
     public void updateText()
     {
         //textBox.text = getModifierText();
-        textBox.text = Camden.getPower().ToString();
+        GainTxtBox.text = powerGain.ToString();
+        LossTxtBox.text = powerLoss.ToString();
+        NetTxtBox.text = Camden.getPower().ToString();
     }
 
 }
